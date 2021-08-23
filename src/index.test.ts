e0dff51a4b7ecc2fc103438a8daa110439c7022f
SciDev5 <jhuber2048@gmail.com>
2021-08-23 15:09:13 -0400

    Configure project, add dependencies

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { exampleFunction } from ".";
-
-test("test exampleFunction",()=>{
-    expect(exampleFunction(4)).toBe(5);
-    expect(exampleFunction(8)).toBe(9);
-    expect(exampleFunction(487)).toBe(488);
-});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-console.log("hello world");
+import express from "express";
 
-export function exampleFunction(x:number):number {
-    return x + 1;
-}
+export const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("It works.");
+});
+
+
+const _portIn = parseInt(process.env.PORT ?? "");
+const port = isFinite(_portIn) ? _portIn : 3000;
+
+export const server = app.listen(port,()=>{
+    console.log(`>> STARTED [port:${port}]`); 
+});
+
+
