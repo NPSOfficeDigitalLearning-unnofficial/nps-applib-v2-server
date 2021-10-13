d8c3bb5490e97d00e39a89e62cf2482bbf9332fa
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2021-10-13 14:55:25 -0400

    change enum values for App.platforms

--- src/data/app/appdata-enums.ts
+++ src/data/app/appdata-enums.ts
@@ -1,6 +1,6 @@
 export const APPROVAL_STATUSES = ["UNK", "APPROVED", "14_PARENTAL_CONSENT", "PARENT_INFORMED", "PILOT", "INSTRUCTOR_ONLY", "PENDING", "DENIED"] as const;
 export const PRIVACY_STATUSES = ["UNK", "COMPLIANT", "NONCOMPLIANT", "PARENTAL_CONSENT", "INSTRUCTOR_ONLY", "NO_INFO_COLLECTED", "NOT_APPLICABLE"] as const;
-export const PLATFORMS = ["WINDOWS", "MACOS", "LINUX", "ANDROID_PHONE", "ANDROID_TABLET", "IOS_PHONE", "IOS_TABLET", "WEB", "CHROMEBOOK"] as const;
+export const PLATFORMS = ["WEB", "MOBILE", "CRX", "PC"] as const;
 export const GRADE_LEVELS = ["PRE_K", "K", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"] as const;
 export const SUBJECTS = ["MATH", "SCIENCE", "HISTORY", "ENGLISH", "LANGUAGE", "CS", "ART", "MUSIC","SEL"] as const;
 
